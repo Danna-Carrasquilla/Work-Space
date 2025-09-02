632b9e43a9cf40fe46084a83d630cb58aba637dc
Danna-Carrasquilla <dannacarrasquillat@ielacandelariamedellin.edu.co>
2025-09-02 16:32:49 -0500

    Add files via upload

---
+++ Septiembre 2/Ejercicio2.js
@@ -0,0 +1,9 @@
+function ejemplowhile(){
+    const k = 7;
+    let j = 0;
+    while(j<=k){
+        alert.log(j);
+        j = j +1;
+    }
+    alert.log("fuera de while",j);
+}
---
+++ Septiembre 2/Ejercicio6.js
@@ -0,0 +1,5 @@
+const nombres = ["Pepito", "Fulanito", "Peranito", "Sultanito"];
+function mostrar(){
+ for(i=0; i<4; i++){
+ const nodohijo = document.createB
+}
